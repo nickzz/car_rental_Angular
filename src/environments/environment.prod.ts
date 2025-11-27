bef6e96059de42671859e53586acb55b9a4c52b5
Hashif <36635662+nickzz@users.noreply.github.com>
2025-11-28 00:59:53 +0800

    fix deploy to render

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://car-rental-8h8v.onrender.com/api/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5052/api/v1'
+  // apiUrl: 'https://localhost:7172/api/v1' //debug api
+
+};
